2c80af8926d9161545cefa5f12bb3b5ffc2d9dd7
ArturMavlidov <artur.mavlidov75@mail.ru>
2022-01-12 03:22:47 +0300

    Made Main.jsx a functional component

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function index() {
     <footer className="page-footer green lighten-1">
       <div className="footer-copyright">
         <div className="container">
-          © {new Date().getFullYear()} Copyright Text
+          © {new Date().getFullYear()} Artur Mavlidov
           <a className="grey-text text-lighten-4 right" href="#!">
             Repo
           </a>
--- src/layout/Main/index.jsx
+++ src/layout/Main/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Movies, Preloader, Search } from "../../components";
 
@@ -6,39 +6,33 @@ import "./style.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-export default class Main extends React.Component {
-  state = {
-    movies: [],
-    loading: true
-  };
+export default function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  setMovies = (str = "avengers", type="all") => {
+  useEffect(() => {
+    addMovies();
+  }, [])
+
+  const addMovies = (str = "avengers", type="all") => {
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => setMovies(data.Search), setLoading(false))
       .catch((err) => {
         console.error(err);
-        this.setState({loading:false});
+        setLoading(false);
       })
   };
 
-  componentDidMount() {
-    this.setMovies();
-  }
-
-  searchMovies = (str, type) => {
-    this.setState({loading: true})
-    this.setMovies(str, type);
+  const searchMovies = (str, type) => {
+    setLoading(true);
+    addMovies(str, type)
   };
-
-  render() {
-    const { movies, loading } = this.state;
 
     return (
       <main className="container content">
-        <Search searchMovies={this.searchMovies} />
+        <Search searchMovies={searchMovies} />
         {loading ? <Preloader /> : <Movies movies={movies} />}
       </main>
     );
-  }
 }
