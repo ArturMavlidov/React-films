3379be170c7ca287554d29b433d0ce4e8bc9e65b
ArturMavlidov <artur.mavlidov75@mail.ru>
2022-04-17 00:54:31 +0300

    Update layout

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import './style.css'
 
 export default function Movies({ Title, Year, imdbID, Type, Poster }) {
+  if (Poster === 'N/A') {
+    console.log(Poster);
+  }
+
   return (
     <div className="card movie">
       <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={Poster} />
+        <img className="activator" src={Poster === 'N/A' ? 'https://via.placeholder.com/250x380' : Poster} />
       </div>
       <div className="card-content">
         <span className="card-title activator grey-text text-darken-4">
